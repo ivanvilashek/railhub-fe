2ce10c2ef22f1884d67107118e4f0dba82571f6f
ivanvilashek <ivan.vilashek@gmail.com>
2024-09-25 16:19:40 +0300

    chore: update root layout

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+import clsx from 'clsx'
+import './globals.css'
+import { Source_Sans_3 } from 'next/font/google'
+import { Providers } from '@app/ui/components/providers'
+
+const sourceSans = Source_Sans_3({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | RailHub',
+    default: 'RailHub',
+  },
+  description: 'RailHub',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={clsx(sourceSans.className, 'h-screen antialiased')}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
