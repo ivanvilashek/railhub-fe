cd005aca8e5aa9a27ba6363c6b66edfc98bca7b3
ivanvilashek <ivan.vilashek@gmail.com>
2024-09-26 13:16:23 +0300

    chore: update hooks

---
+++ app/_lib/hooks/use-click-outside.hook.ts
@@ -0,0 +1,26 @@
+'use client'
+
+import React, { useEffect } from 'react'
+
+export const useClickOutside = (
+  ref: React.RefObject<HTMLDivElement>,
+  handler: (e?: MouseEvent | TouchEvent) => void
+) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return
+      }
+
+      handler(event)
+    }
+
+    document.addEventListener('mousedown', listener)
+    document.addEventListener('touchstart', listener)
+
+    return () => {
+      document.removeEventListener('mousedown', listener)
+      document.removeEventListener('touchstart', listener)
+    }
+  }, [ref, handler])
+}
